ee905fca1b21c02488efb297c13a87d71d2e9846
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2023-12-08 22:25:18 +0700

    fix embedded system

---
+++ camera_esp_32_cam/esp32_camera_mjpeg.ino
@@ -0,0 +1,105 @@
+#include "src/OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+
+#define CAMERA_MODEL_AI_THINKER
+
+#define SSID1 "Mess"
+#define PWD1 "sandinyaanu"
+
+OV2640 cam;
+
+WebServer server(80);
+
+const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
+                      "Access-Control-Allow-Origin: *\r\n" \
+                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
+const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
+const int hdrLen = strlen(HEADER);
+const int bdrLen = strlen(BOUNDARY);
+const int cntLen = strlen(CTNTTYPE);
+
+const int LED_PIN  = 4;
+
+void handle_jpg_stream(void) {
+  char buf[32];
+  int s;
+
+  WiFiClient client = server.client();
+
+  client.write(HEADER, hdrLen);
+  client.write(BOUNDARY, bdrLen);
+
+  while (true) {
+    if (!client.connected()) break;
+    cam.run();
+    s = cam.getSize();
+    client.write(CTNTTYPE, cntLen);
+    sprintf( buf, "%d\r\n\r\n", s );
+    client.write(buf, strlen(buf));
+    client.write((char *)cam.getfb(), s);
+    client.write(BOUNDARY, bdrLen);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // Frame parameters
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+
+  // pinMode(LED_PIN, OUTPUT);
+  // digitalWrite(LED_PIN, 50);
+
+  cam.init(config);
+
+  IPAddress ip;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID1, PWD1);
+  Serial.println("Connecting To WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  ip = WiFi.localIP();
+  Serial.print("WiFi connected");
+  Serial.println("");
+  Serial.println(ip);
+  Serial.print("Stream Link: http://");
+  Serial.print(ip);
+  Serial.println("/K2A/1");
+  server.on("/K2A/1", HTTP_GET, handle_jpg_stream);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
